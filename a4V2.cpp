f8a1e24c87434186517bfcfaf15cf0de8bba338b
bhaskarbagchi <bhaskarbagchi13101992@gmail.com>
2015-03-19 01:09:01 +0530

    Connected components: Not sure about the correctneess. As of now proceed with this result

--- a4V2.cpp
+++ a4V2.cpp
@@ -3,32 +3,54 @@
 #include <cstdio>
 #include <algorithm>
 #include <climits>
+#include <bits/stdc++.h>
 
 #define pb push_back
 
 using namespace std;
 
 class Point3D{
-  int x;
-  int y;
-  int z;
-  Point3D(){}
-  Point3D(int _x, int _y, int _z) : x(_x), y(_y), z(_z){}
+public:
+	int x;
+	int y;
+	int z;
+	Point3D(){}
+	Point3D(int _x, int _y, int _z) : x(_x), y(_y), z(_z){}
+
+	bool operator<(const Point3D &pt) const {
+		if(x<pt.x)
+			return true;
+		if(x == pt.x && y < pt.y)
+			return true;
+		if(x == pt.x && y==pt.y && z < pt.z)
+			return true;
+		return false;
+	}
+
+	bool operator==(const Point3D &pt) const {
+		return x == pt.x && y == pt.y && z == pt.z;
+	}
 };
 
-typedef vector<Point3D> VPoint3D;
+namespace std {
+	template <>
+	struct hash<Point3D> {
+		std::size_t operator()(const Point3D& pt) const {
+			return pt.x*1e6 + pt.y * 1e3 + pt.z;
+		}
+	};
+}
 
+typedef vector<Point3D> VPoint3D;
 typedef vector<VPoint3D> VVPoint3D; // 6Neighbourhood Connected voxels list
 
 VPoint3D voxels;
 VVPoint3D conVoxels;
 
-
 void input(){
 	voxels.clear();
 
 	Point3D tmp;
-
 	int n;
 	scanf("%d",&n);
 	while(n--){
@@ -36,14 +58,65 @@ void input(){
 		voxels.pb(tmp);
 	}
 }
- 
-void connectVoxels(){
 
-
-	
+//There is some memory leak in this function...
+void connectVoxels() {
+	conVoxels.clear();
+	unordered_map<Point3D, int> voxelMap;
+	unordered_map<Point3D, bool> voxelVisited;
+	int sz = voxels.size();
+	for(int i = 0; i < sz; i++) {
+		voxelMap[voxels[i]] = i;
+	}
+	for (int i = 0; i < sz; ++i) {
+		// cout<<"Voxel "<<i<<" visited = "<<voxelVisited[voxels[i]]<<endl;
+		if(!voxelVisited[voxels[i]]) {
+			//BFS
+			vector<Point3D> v;
+			queue<int> q;
+			q.push(voxelMap[voxels[i]]);
+			while(!q.empty()) {
+				Point3D p = voxels[q.front()];
+				// cout<<q.front()<<endl;
+				v.push_back(p);
+				voxelVisited[p] = true;
+				q.pop();
+				if(voxelMap.find(Point3D(p.x-1, p.y, p.z)) != voxelMap.end() && !voxelVisited[Point3D(p.x-1, p.y, p.z)]) {
+					q.push(voxelMap[Point3D(p.x-1, p.y, p.z)]);
+				}
+				if(voxelMap.find(Point3D(p.x+1, p.y, p.z)) != voxelMap.end() && !voxelVisited[Point3D(p.x+1, p.y, p.z)]) {
+					q.push(voxelMap[Point3D(p.x+1, p.y, p.z)]);
+				}
+				if(voxelMap.find(Point3D(p.x, p.y-1, p.z)) != voxelMap.end() && !voxelVisited[Point3D(p.x, p.y-1, p.z)]) {
+					q.push(voxelMap[Point3D(p.x, p.y-1, p.z)]);
+				}
+				if(voxelMap.find(Point3D(p.x, p.y+1, p.z)) != voxelMap.end() && !voxelVisited[Point3D(p.x, p.y+1, p.z)]) {
+					q.push(voxelMap[Point3D(p.x, p.y+1, p.z)]);
+				}
+				if(voxelMap.find(Point3D(p.x, p.y, p.z-1)) != voxelMap.end() && !voxelVisited[Point3D(p.x, p.y, p.z-1)]) {
+					q.push(voxelMap[Point3D(p.x, p.y, p.z-1)]);
+				}
+				if(voxelMap.find(Point3D(p.x, p.y, p.z+1)) != voxelMap.end() && !voxelVisited[Point3D(p.x, p.y, p.z+1)]) {
+					q.push(voxelMap[Point3D(p.x, p.y, p.z+1)]);
+				}
+			}
+			conVoxels.push_back(v);
+		}
+	}
+	for(int i = 0; i < conVoxels.size(); i++) {
+		cout<<"Connected Component #"<<i+1<<" : "<<endl;
+		for(int j = 0; j < conVoxels[i].size(); j++) {
+			cout<<conVoxels[i][j].x<<" "<<conVoxels[i][j].y<<" "<<conVoxels[i][j].z<<endl;
+		}
+	}
+	cout<<"No. of connected components = "<<conVoxels.size()<<endl;
+	for(int i = 0; i < conVoxels.size(); i++) {
+		cout<<"Connected Component #"<<i+1<<" : size = "<<conVoxels[i].size()<<endl;
+	}
 }
 
-
-int main(){
+int main(int argc, char * argv[]) {
+	input();
+	connectVoxels();
 	return 0;
 }
